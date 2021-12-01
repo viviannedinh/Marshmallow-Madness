dbc8a58faf390bf47d9df558c228366666412174
Vivianne Dinh <viviannedinh@ucla.edu>
2021-11-30 23:25:30 -0800

    camera views

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ export class MarshmallowMadness extends Scene {
             s_mug9: new Shape_From_File("assets/mug.obj"),
             cup: new defs.Capped_Cylinder(30, 30),
             marshmallow: new defs.Rounded_Capped_Cylinder(10, 10),
+            s_marshmallow: new defs.Rounded_Capped_Cylinder(10, 10),
             table: new defs.Square()
         };
         
@@ -68,6 +69,9 @@ export class MarshmallowMadness extends Scene {
         }
   
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 50), vec3(0, -10, 0), vec3(0, 1, 0));
+        this.second_player_view = Mat4.look_at(vec3(0, 10, -50), vec3(0, -10, 0), vec3(0, 1, 0));
+
+
     }
 
     make_control_panel() {
@@ -83,16 +87,19 @@ export class MarshmallowMadness extends Scene {
         this.key_triggered_button("collide 8", ["v"], () => { this.mugs[8].colliding = true; });
         this.key_triggered_button("collide 9", ["v"], () => { this.mugs[9].colliding = true; });
         this.new_line();
-        this.key_triggered_button("collide 0", ["v"], () => { this.s_mugs[0].colliding = true; });
-        this.key_triggered_button("collide 1", ["v"], () => { this.s_mugs[1].colliding = true; });
-        this.key_triggered_button("collide 2", ["v"], () => { this.s_mugs[2].colliding = true; });
-        this.key_triggered_button("collide 3", ["v"], () => { this.s_mugs[3].colliding = true; });
-        this.key_triggered_button("collide 4", ["v"], () => { this.s_mugs[4].colliding = true; });
-        this.key_triggered_button("collide 5", ["v"], () => { this.s_mugs[5].colliding = true; });
-        this.key_triggered_button("collide 6", ["v"], () => { this.s_mugs[6].colliding = true; });
-        this.key_triggered_button("collide 7", ["v"], () => { this.s_mugs[7].colliding = true; });
-        this.key_triggered_button("collide 8", ["v"], () => { this.s_mugs[8].colliding = true; });
-        this.key_triggered_button("collide 9", ["v"], () => { this.s_mugs[9].colliding = true; });
+        this.key_triggered_button("s collide 0", ["v"], () => { this.s_mugs[0].colliding = true; });
+        this.key_triggered_button("s collide 1", ["v"], () => { this.s_mugs[1].colliding = true; });
+        this.key_triggered_button("s collide 2", ["v"], () => { this.s_mugs[2].colliding = true; });
+        this.key_triggered_button("s collide 3", ["v"], () => { this.s_mugs[3].colliding = true; });
+        this.key_triggered_button("s collide 4", ["v"], () => { this.s_mugs[4].colliding = true; });
+        this.key_triggered_button("s collide 5", ["v"], () => { this.s_mugs[5].colliding = true; });
+        this.key_triggered_button("s collide 6", ["v"], () => { this.s_mugs[6].colliding = true; });
+        this.key_triggered_button("s collide 7", ["v"], () => { this.s_mugs[7].colliding = true; });
+        this.key_triggered_button("s collide 8", ["v"], () => { this.s_mugs[8].colliding = true; });
+        this.key_triggered_button("s collide 9", ["v"], () => { this.s_mugs[9].colliding = true; });
+        this.new_line();
+        this.key_triggered_button("player 1 turn", ["v"], () => this.attached = () => this.marshmallow_scale);
+        this.key_triggered_button("player 2 turn", ["v"], () => this.attached = () => this.s_marshmallow_scale);
     };
    
     display(context, program_state) {
@@ -100,8 +107,10 @@ export class MarshmallowMadness extends Scene {
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            // Define the global camera and projection matrices, which are stored in program_state.                                                                                                   
+            // Define the global camera and projection matrices, which are stored in program_state.
+            
             program_state.set_camera(this.initial_camera_location);
+            
         }
 
 
@@ -122,7 +131,7 @@ export class MarshmallowMadness extends Scene {
             Mat4.scale(20, 40, 1));
         this.shapes.table.draw(context, program_state, table_scale, this.materials.table_material);
      
-        // first player mugs
+        // player 1 mugs
         let mugs_transform = Mat4.identity().times(Mat4.translation(0, -8.5, -20)).times(Mat4.rotation(2*1.57079633, 0, -1, 0));
         mugs_transform = mugs_transform.times(Mat4.scale(2, 2, 2));
 
@@ -155,7 +164,7 @@ export class MarshmallowMadness extends Scene {
             mugs_transform = mugs_transform.times(Mat4.translation(-2.5 * (i + 0.5), 0, 2.5));    
         }
 
-        // second player mugs
+        // player 2 mugs
         let s_mugs_transform = Mat4.identity().times(Mat4.translation(0, -8.5, 20)).times(Mat4.rotation(2*1.57079633, 0, -1, 0));
         s_mugs_transform = s_mugs_transform.times(Mat4.scale(2, 2, 2));
 
@@ -194,8 +203,31 @@ export class MarshmallowMadness extends Scene {
 //         this.shapes.box.draw(context, program_state, axis_transform.times(Mat4.scale(.1, 10, .1)), this.materials.test.override({color: color(0, 1, 0, 1)}));
 //         this.shapes.box.draw(context, program_state, axis_transform.times(Mat4.scale(.1, .1, 10)), this.materials.test.override({color: color(0, 0, 1, 1)}));
        
-        //marshmallow
-        let marshmallow_scale = Mat4.identity().times(Mat4.scale(1, 1, 1.9)).times(Mat4.translation(0, 0, 20));
-        this.shapes.marshmallow.draw(context, program_state, marshmallow_scale, this.materials.test);
+        // player 1 marshmallow
+        this.marshmallow_scale = Mat4.identity().times(Mat4.scale(1, 1, 1.9)).times(Mat4.translation(0, 0, 20));
+        this.shapes.marshmallow.draw(context, program_state, this.marshmallow_scale, this.materials.test);
+
+        // player 2 marshmallow
+        this.s_marshmallow_scale = Mat4.identity().times(Mat4.scale(1, 1, 1.9)).times(Mat4.translation(0, 0, -20));
+        this.shapes.s_marshmallow.draw(context, program_state, this.s_marshmallow_scale, this.materials.test);
+        this.s_marshmallow_scale = this.s_marshmallow_scale.times(Mat4.rotation(Math.PI, 0, 1, 0));
+        
+        // current player view
+        if (this.attached) {
+            if (this.attached() == this.marshmallow_scale) {
+                const blending_factor = 0.05;
+                const desired = this.attached().times(Mat4.translation(0, 3, 9));
+                const current = desired.map( (x,i) => 
+                    Vector.from( program_state.camera_transform[i] ).mix( x, blending_factor ) );
+                program_state.set_camera(Mat4.inverse(current));
+            }
+            else if (this.attached() == this.s_marshmallow_scale){
+                const blending_factor = 0.05;
+                let desired = this.attached().times(Mat4.translation(0, 3, 9));
+                let current = desired.map( (x,i) => 
+                    Vector.from( program_state.camera_transform[i] ).mix( x, blending_factor ) );
+                program_state.set_camera(Mat4.inverse(current));
+            }
+        }
     }
 }
