75d751bada801d8c3bb6da67307075aa1bb8d46e
Alex Chen <achen2289@gmail.com>
2021-12-01 00:29:53 -0800

    finish projectile motion fixed along z axis

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import {defs, tiny} from './examples/common.js';
-import {Shape_From_File} from "./examples/obj-file-demo.js"
-
+import {Shape_From_File} from "./examples/obj-file-demo.js";
+import {Body, Simulation} from "./examples/collisions-demo.js";
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Texture, Scene
 } = tiny;
 
-export class MarshmallowMadness extends Scene {
+export class MarshmallowMadness extends Simulation {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
@@ -26,18 +26,100 @@ export class MarshmallowMadness extends Scene {
         this.materials = {
             mug_body: new Material(new defs.Textured_Phong(1),
                 {ambient: .7, diffusivity: .5, specularity: 0.5, color: color(0.5, 0.5, 0.5, 1), texture: new Texture("assets/peppermint.jpeg")}),
-            test: new Material(new defs.Phong_Shader(),
+            marshmallow: new Material(new defs.Phong_Shader(),
                 {ambient: .5, diffusivity: 1, color: hex_color("#ffffff")}),
             table_material: new Material(new defs.Textured_Phong(1), {ambient: .9, texture: new Texture("assets/wood.jpg")}),
         }
+        
+        
+        this.max_velocity = 30;
+        this.horizontal_angle = Math.PI / 2; // angle right of -z axis (on xz plane)
+        this.initialize_marshmallow();
 
-
-        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 50), vec3(0, -10, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 70), vec3(0, -10, 0), vec3(0, 1, 0));
     }
 
-    make_control_panel() {};
+    make_control_panel() {
+        this.key_triggered_button("Fire shot", ["Shift"], () => {
+            this.shot_fired = true;
+        });
+        this.new_line();
+        this.key_triggered_button("Increase power", ["i"], () => {
+            if (this.shot_fired != true && this.current_velocity < this.max_velocity) {
+                this.current_velocity += 2;
+            }
+        });
+        this.new_line();
+        this.key_triggered_button("Decrease power", ["k"], () => {
+            if (this.shot_fired != true && this.current_velocity > 0) {
+                this.current_velocity -= 2;
+            }
+        });
+    };
+
+    initialize_marshmallow() {
+        // Initialize variables related to marshmallow and projectile
+        this.bodies = [];
+        this.current_velocity = 0;
+        this.shot_fired = false;
+        this.marshmallow_angle = Math.PI / 3; // angle above -z axis (on yz plane)
+    }
+
+    update_state(dt) {
+        // update_state():  Override the base time-stepping code to say what this particular
+        // scene should do to its bodies every frame -- including applying forces.
+        // Generate additional moving bodies if there ever aren't enough:
+        while (this.bodies.length < 1) {
+            this.bodies.push(
+                new Body(
+                    this.shapes.marshmallow, 
+                    this.materials.marshmallow, 
+                    vec3(1, 1, 1.9)
+                ).emplace(
+                    Mat4.translation(...vec3(0, 0, 50)),
+                    vec3(0, 0, 0), 
+                    0,
+                )
+            )
+        }
+        
+        // If shot was fired, update velocity and position of marshmallow
+        // If marshmallow has collision, reset marshmallow position & power
+        if (this.shot_fired) {
+            for (let b of this.bodies) {
+                // Gravity on Earth, where 1 unit in world space = 1 meter:
+                let z_vel = -this.current_velocity * Math.cos(this.marshmallow_angle);
+                let y_vel = this.current_velocity * Math.sin(this.marshmallow_angle) + dt * -9.8;
+//                 let x_vel = this.current_velocity * Math.cos(this.horizontal_angle);
+                
+                this.current_velocity = Math.pow(
+                    Math.pow(z_vel, 2) + Math.pow(y_vel, 2),
+                    0.5
+                );
+                this.marshmallow_angle = Math.atan(y_vel / -z_vel);
+
+                b.linear_velocity[2] = z_vel;
+                b.linear_velocity[1] = y_vel;
+//                 b.linear_velocity[0] = x_vel;
+
+                // If about to fall through floor, reverse y velocity:
+                // TODO: collision logic and switch camera angles / player
+                if (b.center[1] < -8 && b.linear_velocity[1] < 0) {
+//                     b.linear_velocity[1] *= -.8;
+
+
+                    this.initialize_marshmallow();
+                }
+                b.advance(dt);
+            }
+            // Delete bodies that stop or stray too far away:
+//             this.bodies = this.bodies.filter(b => b.center.norm() < 50 && b.linear_velocity.norm() > 2);
+        }
+    }
 
     display(context, program_state) {
+        super.display(context, program_state);
+        
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
@@ -84,8 +166,8 @@ export class MarshmallowMadness extends Scene {
 //         this.shapes.box.draw(context, program_state, axis_transform.times(Mat4.scale(.1, .1, 10)), this.materials.test.override({color: color(0, 0, 1, 1)}));
        
         //marshmallow
-        let marshmallow_scale = Mat4.identity().times(Mat4.scale(1, 1, 1.9)).times(Mat4.translation(0, 0, 20));
-        this.shapes.marshmallow.draw(context, program_state, marshmallow_scale, this.materials.test);
+//         let marshmallow_scale = Mat4.identity().times(Mat4.scale(1, 1, 1.9)).times(Mat4.translation(0, 0, 20));
+//         this.shapes.marshmallow.draw(context, program_state, marshmallow_scale, this.materials.marshmallow);
 
     }
 }
